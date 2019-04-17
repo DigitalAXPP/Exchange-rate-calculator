0593c6ef27e37b2c5bdb46560e80cff172826702
Alex <44818867+DigitalAXPP@users.noreply.github.com>
2019-04-17 21:52:10 +0200

    Add files via upload

--- Currency Converter_CLI.py
+++ Currency Converter_CLI.py
@@ -6,7 +6,7 @@ import os
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--amount', type=float, default=1750,
+    parser.add_argument('-a', '--amount', type=float, default=[],
                         help='Insert parameter you want to have converted.')
     parser.add_argument('-ci', '--currency_in', type=str, default='eur',
                         help='Insert the currency of your amount.')
@@ -23,20 +23,11 @@ def convert_amount(args):
     amount of another currency or if no second currency was entered,
     the amount will be converted to all available currencies.
 
-    Attributes
-    ----------
-    amount : integer
-        The amount which will be converted
-    currency_in : string
-        The currency from which the amount will be converted.
-    currency_out : string
-        The currency to which the amount will be converted.
-
     Currencies
     ----------
     USD, EUR, GBP, JPY, AUD, CAD, CHF, DKK, AED, CZK, PLN
 
-    Methods
+    Parameters
     -------
         amount = Integer input from which the output will be calculated.
         currency_in = Three character string input. Required to establish from
@@ -45,34 +36,9 @@ def convert_amount(args):
                     output is converted to this parameter. The default is None.
     """
 
-    # Input validation part
-    try:
-        amount = int(args.amount)
-    except (ValueError, NameError):
-        return "Please enter an integer as amount."
-
-    try:
-        if isinstance(args.currency_in, str):
-            currency_in = args.currency_in
-        else:
-            if not isinstance(args.currency_in, str):
-                raise ValueError
-    except ValueError:
-        return "Please enter a three character string as currency."
-
-    try:
-        if args.currency_out is None:
-            currency_out = args.currency_out
-        elif isinstance(args.currency_out, str):
-            currency_out = args.currency_out
-        else:
-            raise KeyError
-    except (ValueError, KeyError):
-        return "Please enter a three character string as currency."
-
     # Setting path to the 'Currency' folder and load the JSON file
     try:
-        folder = os.path.join(os.curdir, '{0}.txt'.format(currency_in))
+        folder = os.path.join(os.curdir, '{0}.txt'.format(args.currency_in))
         with open(folder, 'r') as json_src:
             content = json.load(json_src)
     except FileNotFoundError:
@@ -102,28 +68,28 @@ def convert_amount(args):
     output_dict = dict()
 
     try:
-        data["input"]["amount"] = amount
-        data["input"]["currency"] = currency_in
+        data["input"]["amount"] = args.amount
+        data["input"]["currency"] = args.currency_in
     except Exception as e:
         return e
 
     try:
-        if currency_out is not None:
-            output = round(amount * content[currency_out]["rate"], 2)
-            data["output"] = {content[currency_out]["alphaCode"]: output}
+        if args.currency_out is not None:
+            output = round(args.amount * content[args.currency_out]["rate"], 2)
+            data["output"] = {content[args.currency_out]["alphaCode"]: output}
             out = json.dumps(data, indent=2)
             return out
         else:
             for v in content.values():
-                output = amount * v["rate"]
+                output = args.amount * v["rate"]
                 output_dict[v["alphaCode"]] = output
                 data["output"] = output_dict
-
         out = json.dumps(data, indent=2)
         return out
-
     except (ValueError, KeyError):
         return "Please enter a valid, three character string for your currency."
+    except TypeError:
+        return "Please enter a float as amount."
 
 if __name__ == '__main__':
     main()
